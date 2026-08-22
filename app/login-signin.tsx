aef71d52288c0826b4725b8d1db40a1de533a928
Meng Siau <mengsiau@gmail.com>
2026-08-22 18:52:33 +1000

    Created login-signin route

---
+++ app/login-signin.tsx
@@ -0,0 +1,8 @@
+// Used as a route to render the LoginSignIn screen.
+// IMPORTANT -> filename here acts as the routh path
+
+import { LoginSignIn } from '@/src/screens/LoginSignIn/LoginSignIn';
+
+export default function LoginSignInRoute() {
+  return <LoginSignIn />;
+}
