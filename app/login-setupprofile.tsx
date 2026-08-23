fe79c497397cdde3d67e2932ba37b9a84e9df003
Meng Siau <mengsiau@gmail.com>
2026-08-23 15:08:10 +1000

    setupprofile route

---
+++ app/login-setupprofile.tsx
@@ -0,0 +1,8 @@
+// Used as a route to render the LoginSetUpProfile screen.
+// IMPORTANT -> filename here acts as the route path
+
+import { LoginSetUpProfile } from '@/src/screens/LoginSetUpProfile/LoginSetUpProfile';
+
+export default function LoginSetUpProfileRoute() {
+  return <LoginSetUpProfile />;
+}
