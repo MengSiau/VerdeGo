0739c1092f6d643f758e1e2cc901746624a0d977
srainbow9 <rainbow.t.samuel@gmail.com>
2026-08-25 16:04:51 +1000

    feat: added email verification screen

---
+++ app/verify-email.tsx
@@ -0,0 +1,8 @@
+// Used as a route to render the VerifyEmail screen.
+// IMPORTANT -> filename here acts as the route path
+
+import { VerifyEmail } from '@/src/screens/SignUp/VerifyEmail';
+
+export default function VerifyEmailRoute() {
+  return <VerifyEmail />;
+}
