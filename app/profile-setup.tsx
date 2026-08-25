446cdc3b380560630b2981e2d88342b83e1d7343
srainbow9 <rainbow.t.samuel@gmail.com>
2026-08-25 16:05:13 +1000

    feat: added profile setup screen

---
+++ app/profile-setup.tsx
@@ -0,0 +1,8 @@
+// Used as a route to render the ProfileSetup screen.
+// IMPORTANT -> filename here acts as the route path
+
+import { ProfileSetup } from '@/src/screens/SignUp/ProfileSetup';
+
+export default function ProfileSetupRoute() {
+  return <ProfileSetup />;
+}
