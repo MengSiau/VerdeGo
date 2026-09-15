512eb0f3d32937d9a4a224986f69bf9e5c1398cc
Ayush Sharma <asha0300@student.monash.edu>
2026-09-15 22:30:18 +1000

    Made minor tweaks to the layout and updated the .gitignore to include environment files.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
-import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack, useRouter, useSegments } from 'expo-router';
+import { Stack, useRouter, useSegments, DefaultTheme, ThemeProvider} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
