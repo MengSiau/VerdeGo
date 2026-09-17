ba6f893a6da4c069c3b312cee027c5175451393f
srainbow9 <rainbow.t.samuel@gmail.com>
2026-09-17 13:07:29 +1000

    Added MyVehicles and VehicleForms Pages

---
+++ app/my-vehicles.tsx
@@ -0,0 +1,8 @@
+// Used as a route to render the MyVehicles screen.
+// IMPORTANT -> filename here acts as the route path
+
+import { MyVehicles } from '@/src/screens/MyVehicles/MyVehicles';
+
+export default function MyVehiclesRoute() {
+  return <MyVehicles />;
+}
